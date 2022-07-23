ec37f03d0e481e167b06ce3b943d69889aa6030d
TheMateeus <mateusjsilva16@gmail.com>
2022-07-23 11:53:35 -0300

    Meu portifolio

---
+++ src/assets/js/jqueryHabilidades.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function(){
+
+$('.checkbox').click(function() {
+    $('.menu2').toggle();
+})
+$('.menu-itens2').click(function(){
+        $('.menu2').hide();
+})
+$('.icon').hover(function(){
+    $('.text').toggle();
+})
+$('.icon1').hover(function(){
+    $('.text1').toggle();
+})
+$('.icon2').hover(function(){
+    $('.text2').toggle();
+})
+$('.icon3').hover(function(){
+    $('.text3').toggle();
+})
+$('.icon4').hover(function(){
+    $('.text4').toggle();
+})
+$('.icon5').hover(function(){
+    $('.text5').toggle();
+})
+
+});
